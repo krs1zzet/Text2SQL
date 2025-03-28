72b745d56c1f410c663f04026ccbf066553a630e
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-03-28 15:55:37 +0300

    inital commit

---
+++ src/test/java/cmdotenter/text2sql/Text2sqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cmdotenter.text2sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Text2sqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
